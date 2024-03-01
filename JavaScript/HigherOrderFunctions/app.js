b1b58cadc38369b7703ce65c3cb583478670f6cc
lsh <salee2@student.42seoul.kr>
2024-03-01 15:39:48 +0900

    factory function

--- JavaScript/HigherOrderFunctions/app.js
+++ JavaScript/HigherOrderFunctions/app.js
@@ -14,4 +14,30 @@ function rollDie (){
     console.log(roll);
 }
 
-callTwice(rollDie);
+// callTwice(rollDie);
+
+function makeMysteryFunc(){
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return function () {
+            console.log("CONGRATS, I AM A GOOD FUNCTION!");
+            console.log("YOU WIN A MILLION DOLLARS!");
+        };
+    } else {
+        return function(){
+            alert("YOU HAVE BEEN INFECTED BY A COMPUTER VIRUS!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+            alert("STOP TRYING TO CLOSE THIS WINDOW!");
+        }
+    }
+}
+
+function makeBetweenFunc(min, max) {
+    return function (num){
+        return num >= min && num <= max;
+    }
+ }

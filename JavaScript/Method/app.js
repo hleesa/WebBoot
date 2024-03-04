d854f821417bef20dd42dcce9fd0b62b1b314fb8
lsh <salee2@student.42seoul.kr>
2024-03-04 12:51:42 +0900

    method

---
+++ JavaScript/Method/app.js
@@ -0,0 +1,20 @@
+
+const MyMath={
+    PI: 3.14159,
+    square: function (num) {
+        return num * num;
+    },
+    cube: function (num) {
+        return num ** 3;
+    },
+}
+
+const MyMathShort ={
+    PI: 3.14159,
+    square(num) {
+        return num * num;
+    },
+    cube(num) {
+        return num ** 3;
+    },
+}

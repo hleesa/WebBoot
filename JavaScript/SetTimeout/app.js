3b612c62ba8087e2fc9d8a5fc9780e3e2f64149e
lsh <salee2@student.42seoul.kr>
2024-03-06 12:45:34 +0900

    setTimeout, setInterval

--- JavaScript/SetTimeout/app.js
+++ JavaScript/SetTimeout/app.js
@@ -1,7 +1,22 @@
-console.log("Sending request to server!");
+// console.log("Sending request to server!");
+//
+// setTimeout(() => {
+//     console.log("Here is your data from the server...");
+// }, 3000);
+//
+// console.log("I AM AT THE END OF THE FILE!");
 
-setTimeout(() => {
-    console.log("Here is your data from the server...");
-}, 3000);
 
-console.log("I AM AT THE END OF THE FILE!");
+// console.log("HELLO!!!...");
+// setTimeout(() => {
+//     console.log("..are you still there?");
+// }, 3000);
+//
+// console.log("GOODBYE!!");
+
+
+const id = setInterval(() =>{
+    console.log(Math.random())
+}, 2000)
+
+// clearInterval(id);

a14a86e4d41f8566e1ce93a2414a09f87f082a28
lsh <salee2@student.42seoul.kr>
2024-03-06 11:03:45 +0900

    return arrow function

--- JavaScript/ArrowFunction/app.js
+++ JavaScript/ArrowFunction/app.js
@@ -39,6 +39,33 @@ const square = num => {
     return num * num;
 };
 
-const rollDie = () =>{
-    return Math.floor(Math.random() * 6) + 1;
-}
+// const rollDie = () =>{
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+);
+
+const add2 = (a, b) => a + b;
+
+const isEven1 = function (num) {  //regular function expression
+    return num % 2 === 0;
+};
+
+const isEven2 = (num) => { //arrow function with parens around param
+    return num % 2 === 0;
+};
+
+const isEven3 = num => { //no parens around param
+    return num % 2 === 0;
+};
+
+const isEven4 = num => ( // implicit return
+    num % 2 === 0
+);
+
+const isEven5 = num => num % 2 === 0;
+
+
+

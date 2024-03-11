43d4dfd4fe162bcd470d526d2b322e4d9c4fec9b
lsh <salee2@student.42seoul.kr>
2024-03-11 13:59:29 +0900

    promise intro

--- JavaScript/PromiseIntro/app.js
+++ JavaScript/PromiseIntro/app.js
@@ -9,34 +9,69 @@ const fakeRequestCallback = (url, success, failure) => {
     }, delay);
 };
 
+// const fakeRequestPromise = (url) => {
+//     return new Promise((resolve, reject) => {
+//         const delay = Math.floor(Math.random() * 4500) + 500;
+//         setTimeout(() => {
+//             if (delay > 4000) {
+//                 reject("Connection Timeout :(");
+//             } else {
+//                 resolve(`Here is your fake date from ${url}`);
+//             }
+//         }, delay);
+//     });
+// };
+
 const fakeRequestPromise = (url) => {
     return new Promise((resolve, reject) => {
-        const delay = Math.floor(Math.random() * 4500) + 500;
+        const delay = Math.floor(Math.random() * (4500)) + 500;
         setTimeout(() => {
             if (delay > 4000) {
-                failure("Connection Timeout :(");
+                reject('Connection Timeout :(')
             } else {
-                success(`Here is your fake date from ${url}`);
+                resolve(`Here is your fake data from ${url}`)
             }
-        }, delay);
-    });
-};
+        }, delay)
+    })
+}
 
-fakeRequestCallback('books.com/page1',
-    function (response) {
-        console.log("IT WORKED! ", response);
-        fakeRequestCallback('bokks.com/page2',
-            function (response) {
-                console.log("IT WORKED AGAIN!", response);
-                fakeRequestCallback('bokks.com/page3',
-                    function (response) {
-                        console.log("IT WORKED AGAIN (3rd req) !", response);
-                    }, function (err) {
-                        console.log("ERROR (3rd req)!", err);
+// fakeRequestCallback('books.com/page1',
+//     function (response) {
+//         console.log("IT WORKED! ", response);
+//         fakeRequestCallback('bokks.com/page2',
+//             function (response) {
+//                 console.log("IT WORKED AGAIN!", response);
+//                 fakeRequestCallback('bokks.com/page3',
+//                     function (response) {
+//                         console.log("IT WORKED AGAIN (3rd req) !", response);
+//                     }, function (err) {
+//                         console.log("ERROR (3rd req)!", err);
+//                     });
+//             }, function (err) {
+//                 console.log("ERROR (2nd req)!", err);
+//             });
+//     }, function (err) {
+//         console.log("ERROR! ", err);
+//     });
+
+fakeRequestPromise('x.com/api/page1')
+    .then(() => {
+        console.log("IT WORK! (page1)");
+        fakeRequestPromise('x.com/api/page2')
+            .then(() => {
+                console.log("IT WORK! (page2)");
+                fakeRequestPromise('x.com/api/page3')
+                    .then(() => {
+                        console.log("IT WORK! (page3)");
+                    })
+                    .catch(() => {
+                        console.log("OH NO, ERROR (page3)");
                     });
-            }, function (err) {
-                console.log("ERROR (2nd req)!", err);
+            })
+            .catch(() => {
+                console.log("OH NO, ERROR (page2)");
             });
-    }, function (err) {
-        console.log("ERROR! ", err);
+    })
+    .catch(() => {
+        console.log("OH NO, ERROR (page1)");
     });
